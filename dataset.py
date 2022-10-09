21f650fe55311dbb44100ca0567cfb25caf97b95
Niki Kiedanski <nicole@192.168.1.5>
2022-10-09 17:15:42 -0300

    null values were looked for

--- dataset.py
+++ dataset.py
@@ -2,16 +2,17 @@
 import pandas as pd
 import numpy as np
 
-pd.set_option("display.max_columns", None)
-
 wine_red = pd.read_csv("winequality-red.csv", sep=";")
+print("Wine Red dataset", "\n", wine_red)
 wine_white = pd.read_csv("winequality-white.csv", sep=";")
+print("Wine White dataset", "\n", wine_white)
 
-print("The shape of red wine file is: ", wine_red.shape, "\n"
-      "The shape of white wine file is: ", wine_white.shape)
+print("The shape of red wine file is: ", "rows: ", wine_red.shape[0], "columns: ", wine_red.shape[1], "\n"
+      "The shape of white wine file is: ", "rows: ", wine_white.shape[0], "columns: ", wine_white.shape[1], "\n")
 
 print("Is there any null value in the red wine data?: ", wine_red.isnull().values.any(), "\n"
-      "Is there any null value in the white wine data?: ", wine_white.isnull().values.any())
+      "Is there any null value in the white wine data?: ", wine_white.isnull().values.any(), "\n")
 
-
+print("Are the same variables being measure?: ", wine_red.columns.values.tolist() == wine_white.columns.values.tolist())
+print("The wine variables are: ", *wine_red.columns.values.tolist(), sep="\n")
 
