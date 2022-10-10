5df5f7696fe2593c0783c7f11400263b532a7d7b
Niki Kiedanski <nicole@Nikis-MacBook-Pro.local>
2022-10-10 18:21:53 -0300

    the complete merged df was scaled before doing the plots, then concatenated the non numeric columns

--- dataset.py
+++ dataset.py
@@ -19,7 +19,7 @@ print("Is there any missing value in the red wine data?: ", wine_red.isnull().va
       "Is there any missing value in the white wine data?: ", wine_white.isnull().values.any(), "\n")
 
 print("Are the same variables being measure?: ", wine_red.columns.values.tolist() == wine_white.columns.values.tolist())
-print("The wine variables are: ", *wine_red.columns.values.tolist(), "\n", sep="\n", )
+print("**The wine variables are:** ", *wine_red.columns.values.tolist(), "\n", sep="\n", )
 
 print("------------ cleaning ------------ ", "\n")
 
@@ -36,64 +36,39 @@ merged_df.loc[merged_df.loc[:, "quality"] <= 3, "category"] = "low"
 
 print("Complete Wine Dataset", "\n", merged_df, "\n")
 
-stat_description_type = merged_df.groupby("type")[["citric acid", "residual sugar", "density", "pH", "alcohol"]].\
-    agg([np.mean, np.std])
+taste_variables = ["citric acid", "residual sugar", "density", "alcohol"]
+stat_description_type = merged_df.groupby("type")[taste_variables].agg([np.mean, np.std])
 print("Statistical description by type: red/white", "\n", stat_description_type, "\n")
 
-stat_description_category = merged_df.groupby("category")[["fixed acidity", "volatile acidity", "citric acid",
-                                                           "residual sugar", "chlorides", "total sulfur dioxide",
-                                                           "density", "pH", "sulphates", "alcohol"]].agg([np.mean, np.std])
+quality_variables = ["fixed acidity", "volatile acidity", "citric acid", "residual sugar", "chlorides",
+                     "total sulfur dioxide", "density", "pH", "sulphates", "alcohol"]
+stat_description_category = merged_df.groupby("category")[quality_variables].agg([np.mean, np.std])
 print("Statistical description by category: low/mid/high", "\n", stat_description_category, "\n")
 
+# Scaled dataset
+scaler = MinMaxScaler()
+merged_df_scaled = scaler.fit_transform(merged_df[quality_variables].to_numpy())
+merged_df_scaled = pd.DataFrame(merged_df_scaled, columns=quality_variables)
+merged_df_scaled_complete = pd.concat([merged_df_scaled, merged_df[["quality", "type", "category"]]], axis=1)
+print("Complete Wine Dataset Scaled", "\n", merged_df_scaled_complete, "\n")
 
 
 # GRAPHICS RELATED TO CATEGORY
-scaler = MinMaxScaler()
-x_low = merged_df[merged_df["category"] == "low"][["fixed acidity", "volatile acidity", "citric acid", "residual sugar",
-                                               "chlorides", "total sulfur dioxide", "density", "pH", "sulphates",
-                                               "alcohol"]]
-x_low_scaled = scaler.fit_transform(x_low.to_numpy())
-x_low_scaled = pd.DataFrame(x_low_scaled)
+x_low = merged_df_scaled_complete[merged_df_scaled_complete["category"] == "low"][quality_variables]
+x_mid = merged_df_scaled_complete[merged_df_scaled_complete["category"] == "mid"][quality_variables]
+x_high = merged_df_scaled_complete[merged_df_scaled_complete["category"] == "high"][quality_variables]
 
-x_mid = merged_df[merged_df["category"] == "mid"][["fixed acidity", "volatile acidity", "citric acid", "residual sugar",
-                                               "chlorides", "total sulfur dioxide", "density", "pH", "sulphates",
-                                               "alcohol"]]
-x_mid_scaled = scaler.fit_transform(x_mid.to_numpy())
-x_mid_scaled = pd.DataFrame(x_mid_scaled)
-
-x_high = merged_df[merged_df["category"] == "high"][["fixed acidity", "volatile acidity", "citric acid", "residual sugar",
-                                                 "chlorides", "total sulfur dioxide", "density", "pH", "sulphates",
-                                                 "alcohol"]]
-x_high_scaled = scaler.fit_transform(x_high.to_numpy())
-x_high_scaled = pd.DataFrame(x_high_scaled)
 
 # PLOT GRAPHS
-
-
-
-# DE BARRAS
+fig = plt.figure()
 x = np.arange(len(x_low.columns))
-height_low = x_low_scaled.mean()
-height_mid = x_mid_scaled.mean()
-height_high = x_high_scaled.mean()
-# yerr = 1.96*x_low.std()/math.sqrt(len(x_low.index))
-# yerr2 = 1.96*x_mid.std()/math.sqrt(len(x_mid.index))
-# fig, ax = plt.subplots(figsize=(12,12))
-# ax.bar(x, height, yerr=yerr, align='center', alpha=0.5, ecolor='black', capsize=10)
-# ax.bar(x, height2, yerr=yerr2, align='center', alpha=0.5, ecolor='black', capsize=10)
-# ax.set_ylabel("Mean of wine variables +- std")
-# ax.set_xticks(x)
-# ax.set_xticklabels(x_low.columns.tolist())
-# ax.set_title("Wine variables per category")
-# ax.yaxis.grid(True)
-# plt.tight_layout()
-# plt.show()
-
-
+y_low = x_low.mean()
+y_mid = x_mid.mean()
+y_high = x_high.mean()
 ax = plt.axes()
-plt.plot(x, height_low, linewidth=0.5, color="magenta", label="low category")
-plt.plot(x, height_mid, linewidth=0.5, color="blue", label="mid category")
-plt.plot(x, height_high, linewidth=0.5, color="green", label="high category")
+plt.plot(x, y_low, linewidth=0.5, color="magenta", label="low category")
+plt.plot(x, y_mid, linewidth=0.5, color="blue", label="mid category")
+plt.plot(x, y_high, linewidth=0.5, color="green", label="high category")
 ax.set_xticks(x)
 ax.legend()
 ax.set_xticklabels(x_low.columns.tolist(), fontsize=4.5)
@@ -101,5 +76,5 @@ ax.set_xticklabels(x_low.columns.tolist(), fontsize=4.5)
 # Adding title to ax and figure
 plt.title("Wine variables per category", fontsize=10)
 plt.xlabel("Wine variables")
-plt.ylabel("Mean of wine variables +- std")
+plt.ylabel("Mean of wine variables")
 plt.show()
