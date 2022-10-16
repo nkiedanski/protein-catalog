f541c7beed708ff73f4ffcd0410b4229e437d643
Niki Kiedanski <nicole@192.168.1.6>
2022-10-16 12:42:27 -0300

    funtion add protein added with test

--- homework3/database.py
+++ homework3/database.py
@@ -34,7 +34,43 @@ class Database:
             list_proteins.append(protein)
         return list_proteins
 
+    def retrieve_protein_id(self, pdb_code):
+        list_proteins = []
+        if self.__df["--PDBcode--"].isin([pdb_code]).any():
+            index_pdb_code = int(self.__df[self.__df["--PDBcode--"] == pdb_code].index.values)
+            id = self.__df.iloc[index_pdb_code, 0]
+            classification = self.__df.iloc[index_pdb_code, 1]
+            organism = self.__df.iloc[index_pdb_code, 2]
+            year_deposited = self.__df.iloc[index_pdb_code, 3]
+            manually_curated = bool(self.__df.iloc[index_pdb_code, 4])
+            atom_count = self.__df.iloc[index_pdb_code, 5]
+            protein = Protein(id, classification, organism, year_deposited, manually_curated, atom_count)
+            list_proteins.append(protein)
+        return list_proteins
 
+    def retrieve_protein_classification(self, classification):
+        list_proteins = []
+        if self.__df["--Classification--"].isin([classification]).any():
+            index_classification = self.__df[self.__df["--Classification--"] == classification].index.values.tolist()
+            for index in index_classification:
+                id = self.__df.iloc[index, 0]
+                classification = self.__df.iloc[index, 1]
+                organism = self.__df.iloc[index, 2]
+                year_deposited = self.__df.iloc[index, 3]
+                manually_curated = bool(self.__df.iloc[index, 4])
+                atom_count = self.__df.iloc[index, 5]
+                protein = Protein(id, classification, organism, year_deposited, manually_curated, atom_count)
+                list_proteins.append(protein)
+        return list_proteins
+
+    def add_protein(self, protein):
+        new_row = {"--PDBcode--": protein.get_pdb_code(), "--Classification--": protein.get_classification(),
+                   "--Organism--": protein.get_organism(), "--YearDeposited--": protein.get_year_deposited(),
+                   "--ManuallyCrurated--": protein.get_manually_curated(), "--AtomCount--": protein.get_atom_count()}
+        self.__df = self.__df.append(new_row, ignore_index=True)
+        self.__df.to_csv("proteins.csv", sep=";", index=False, mode="w")
+
+    def delete_protein(self, protein):
 
 
 
--- homework3/test_database.py
+++ homework3/test_database.py
@@ -1,9 +1,21 @@
 import unittest
+
+import pandas as pd
+
 from database import Database
 from protein import Protein
 
 
 class TestCatalog(unittest.TestCase):
+    def setUp(self):
+        data = {"--PDBcode--": pd.Series(["3C49", "3C48", "4LTR"]),
+                "--Classification--": pd.Series(["transferasa", "transferasa", "transport protein"]),
+                "--Organism--": pd.Series(["Homo sapiens", "Homo sapiens", "Alkalilimnicola ehrlichii"]),
+                "--YearDeposited--": pd.Series(["2008-01-29", "2004-01-29", "2013-07-23"]),
+                "--ManuallyCrurated--": pd.Series(["True", "True", "True"]),
+                "--AtomCount--": pd.Series(["357", "508", "152"])}
+        base_dataframe = pd.DataFrame(data)
+        base_dataframe.to_csv("proteins.csv", sep=";", index=False, mode="w")
 
     def test_retrieve_proteins(self):
         my_database = Database()
@@ -19,8 +31,34 @@ class TestCatalog(unittest.TestCase):
         list_proteins = [alfa]
         self.assertEqual(my_database.retrieve_protein_id("3C49"), list_proteins)
 
+    def test_retrieve_protein_classification(self):
+        my_database = Database()
+        alfa = Protein("3C49", "transferasa", "Homo sapiens", "2008-01-29", True, "357")
+        beta = Protein("3C48", "transferasa", "Homo sapiens", "2004-01-29", True, "508")
+        list_proteins = [alfa, beta]
+        self.assertEqual(my_database.retrieve_protein_classification("transferasa"), list_proteins)
 
+    def test_add_protein(self):
+        my_database = Database()
+        alfa = Protein("NIC", "transferasa", "Homo sapiens", "2008-01-29", True, "357")
+        my_database.add_protein(alfa)
+        self.assertEqual(my_database.retrieve_protein_id("NIC"), [alfa])
 
+    def test_add_protein_modify_dataset(self):
+        my_database = Database()
+        alfa = Protein("NIC", "transferasa", "Homo sapiens", "2008-01-29", True, "357")
+        my_database.add_protein(alfa)
+        new_database = Database()
+        self.assertEqual(new_database.retrieve_protein_id("NIC"), [alfa])
+
+    def test_delete_protein(self):
+        my_database = Database()
+        alfa = Protein("3C49", "transferasa", "Homo sapiens", "2008-01-29", True, "357")
+        beta = Protein("3C48", "transferasa", "Homo sapiens", "2004-01-29", True, "508")
+        gama = Protein("4LTR", "transport protein", "Alkalilimnicola ehrlichii", "2013-07-23", True, "152")
+        my_database.delete_protein(alfa)
+        list_proteins = [beta, gama]
+        self.assertEqual(my_database.retrieve_proteins(), list_proteins)
 
 if __name__ == '__main__':
         unittest.main()
